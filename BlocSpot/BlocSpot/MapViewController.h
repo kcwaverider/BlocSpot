282b487c39658f2d00c4768a3fdcf6758d54d433
Chad Clayton <chad@kcwaverider.com>
2015-10-08 15:57:10 -0500

    Added map and inital locationi w/ zoom

---
+++ BlocSpot/BlocSpot/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  BlocSpot
+//
+//  Created by Chad Clayton on 10/8/15.
+//  Copyright © 2015 Chad Clayton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController 
+
+@end
---
+++ BlocSpot/BlocSpot/MapViewController.m
@@ -0,0 +1,60 @@
+//
+//  MapViewController.m
+//  BlocSpot
+//
+//  Created by Chad Clayton on 10/8/15.
+//  Copyright © 2015 Chad Clayton. All rights reserved.
+//
+
+#import "MapViewController.h"
+#import <MapKit/MapKit.h>
+#import <CoreLocation/CoreLocation.h>
+
+@interface MapViewController ()  <CLLocationManagerDelegate>
+
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (strong, nonatomic) CLLocationManager *locationManager;
+
+@end
+
+#define METERS_PER_MILE 1609.344
+
+
+@implementation MapViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    _locationManager = [[CLLocationManager alloc] init];
+    _locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    _locationManager.delegate = self;
+    [self.locationManager requestWhenInUseAuthorization];
+    [_locationManager startUpdatingLocation];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void) viewWillAppear:(BOOL)animated {
+    
+    CLLocationCoordinate2D zoomLocation;
+    zoomLocation.latitude = -36.8406;
+    zoomLocation.longitude = 174.7400;
+    
+    MKCoordinateRegion viewRegion = MKCoordinateRegionMakeWithDistance(zoomLocation, .5 * METERS_PER_MILE, .5 * METERS_PER_MILE);
+    [self.mapView setRegion:viewRegion animated:YES];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
