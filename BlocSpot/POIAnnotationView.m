e41f0c8cafc8c77a0ec8d68c1e1247dd9e712c66
Chad Clayton <chad@kcwaverider.com>
2016-01-06 11:20:03 -0600

    Created Interface Builder xib file

---
+++ BlocSpot/POIAnnotationView.m
@@ -0,0 +1,21 @@
+//
+//  POIAnnotationView.m
+//  BlocSpot
+//
+//  Created by Chad Clayton on 1/5/16.
+//  Copyright © 2016 Chad Clayton. All rights reserved.
+//
+
+#import "POIAnnotationView.h"
+
+@implementation POIAnnotationView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
